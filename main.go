af8c66141951b1400f26ac3c789f406e2240e48b
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-05-01 11:46:12 +0900

    if

--- main.go
+++ main.go
@@ -5,20 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println(repeat(15, 13, 11, 9, 7))
+	fmt.Println(isDrink(17))
 }
 
-func repeat(numbers ...int) int {
-	total := 0
-
-	for index, number := range numbers {
-		fmt.Println(index, number)
-		total += number
+func isDrink(age int) bool {
+	if kAge := age + 1; kAge < 18 {
+		return false
 	}
-
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(i, numbers[i])
-	}
-
-	return total
+	return true
 }
