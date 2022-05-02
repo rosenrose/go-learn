9394769ab0682df7875bc6f7f315571c5bfce446
rosenrose <crazytempler@gmail.com>
2022-05-02 13:49:40 +0900

    account

--- main.go
+++ main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/rosenrose/go-learn/banking"
 )
 
-type person struct {
-	name string
-	age int
-	food []string
-}
-
 func main() {
-	man := person {"mike", 20, []string {"a", "b"}}
-	woman := person {age: 25, name: "yolo"}
-	a := person {}
-
-	fmt.Println(man, man.name, woman)
-	fmt.Println(a)
+	account := banking.Account {Owner: "Hans", Balance: 300}
+	fmt.Println(account)
 }
