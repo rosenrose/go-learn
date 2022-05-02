7cc7ca388d37ca7689e187d306c9b53fbd0df045
rosenrose <crazytempler@gmail.com>
2022-05-02 14:34:16 +0900

    methods

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,12 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
+var errNoMoney = errors.New("cant't withdraw")
+
 // Account struct
 type Account struct {
 	owner   string
@@ -10,4 +17,26 @@ type Account struct {
 func NewAccount(owner string) *Account {
 	account := Account {owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit x amount
+func (a *Account) Deposit(amount int) {
+	fmt.Println("Deposit", amount)
+	a.balance += amount
+}
+
+// Balance of account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+
+	fmt.Println("Withdraw", amount)
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -8,5 +8,14 @@ import (
 
 func main() {
 	account := accounts.NewAccount("Hans")
-	fmt.Println(*account)
+	account.Deposit(300)
+	fmt.Println(account.Balance())
+	
+	err := account.Withdraw(500)
+	if err != nil {
+		// log.Fatalln(err)
+		fmt.Println(err)
+	}
+	err = account.Withdraw(200)
+	fmt.Println(account.Balance(), err)
 }
